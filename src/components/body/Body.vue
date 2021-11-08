8b92701b04dcd73b3ed11c528ecc80d1d7ce5bc1
ic5594 <ic5594@naver.com>
2021-11-08 18:03:25 +0900

    Docs:  Body 컴포넌트 간소화

---
+++ src/components/body/Body.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app2">
+        <Imageslider></Imageslider>
+        <Kidservice></Kidservice>
+        <Gnianime></Gnianime>
+        <Event></Event>
+    </div>
+</template>
+<script>
+import Imageslider from './Imageslider.vue'
+import Kidservice from './Kidservice.vue'
+import Gnianime from './Gnianime.vue'
+import Event from './Event.vue'
+
+export default {
+    components:{
+    'Imageslider':Imageslider,
+     'Kidservice':Kidservice,
+     'Gnianime':Gnianime,
+     'Event':Event
+    }
+}
+</script>
+<style>
+
+
+</style>
