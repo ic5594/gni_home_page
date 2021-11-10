8a7c53dd5a2462d28144127201a5637efb2967ff
ic5594 <ic5594@naver.com>
2021-11-10 17:39:50 +0900

    Feat: import from 구문으로 라우팅할 컴포넌트 속성 연결

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,80 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Turnover from '../views/dropdown/Turnover.vue'
+import Starmonmaster from '../views/dropdown/Starmonmaster.vue'
+import Legendofhero from '../views/dropdown/Legendofhero.vue'
+import Monsteroffencehero from '../views/dropdown/Monsteroffencehero.vue'
+import Goldrunner from '../views/dropdown/Goldrunner.vue'
+import Toyracing from '../views/dropdown/Toyracing.vue'
+import Kids from '../views/menu/Kids.vue'
+import Comics from '../views/menu/Comics.vue'
+import Event from '../views/menu/Event.vue'
+import Faq from '../views/menu/Faq.vue'
 
 Vue.use(VueRouter)
-
-const routes = [{
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-
-]
 
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes
-})
+    routes: [{
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/turnover',
+            name: 'Turnover',
+            component: Turnover
+        },
+        {
+            path: '/starmonmaster',
+            name: 'Starmonmaster',
+            component: Starmonmaster
+        },
+        {
+            path: '/legendofhero',
+            name: 'Legendofhero',
+            component: Legendofhero
+        },
+        {
+            path: '/monsteroffencehero',
+            name: 'Monsteroffencehero',
+            component: Monsteroffencehero
+        },
+        {
+            path: '/goldrunner',
+            name: 'Goldrunner',
+            component: Goldrunner
+        },
+        {
+            path: '/toyracing',
+            name: 'Toyracing',
+            component: Toyracing
+        },
+        {
+            path: '/kids',
+            name: 'Kids',
+            component: Kids
+        },
+        {
+            path: '/comics',
+            name: 'Comics',
+            component: Comics
+        },
+        {
+            path: '/event',
+            name: 'Event',
+            component: Event
+        },
+        {
+            path: '/faq',
+            name: 'Faq',
+            component: Faq
+        },
+
+    ]
+
+});
 
 export default router
