afb4a9d771fddb8f53af93baebd3b50fb999ccc8
ic5594 <ic5594@naver.com>
2021-11-10 17:32:36 +0900

    Docs: 라우터를 위한 파일 생성 및 코드 수정

---
+++ src/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id='index'>
+    <Body></Body>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Body from './body/Body.vue'
+import Footer from './footer/Footer.vue'
+
+export default {
+  name:'Index',
+  components:{
+    'Body':Body,
+    'Footer':Footer
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
