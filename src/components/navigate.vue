2a5db1aca77f760f5ada58640f5a377993635ed7
ic5594 <ic5594@naver.com>
2021-11-11 18:00:14 +0900

    Feat: navigate공통 컴포넌트 생성

---
+++ src/components/navigate.vue
@@ -0,0 +1,71 @@
+<template>
+   <div id="navi">
+        <div v-bind:key="number in [currentNumber]" transition="fade">
+            <navi-gate id="navi-gate">
+                <img :src="images[currentNumber%images.length]" witdh="800px" height="600px"/>
+            </navi-gate>
+                <span v-on:click="next" id="next-img">&lang;</span>
+                <span v-on:click="prev" id="prev-img">&rang;</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    el: 'image-slider',
+    name:"starbody3",
+    props:'navGate',
+    data: function(){
+        return{
+            images: ['navGate'],
+            currentNumber: 0,
+            timer:null,
+            };
+    },
+
+    methods: {
+        next: function() {
+            this.currentNumber += 1
+        },
+        prev: function() {
+            this.currentNumber -= 1
+        }
+    }
+
+}
+</script>
+<style>
+    #next-img{
+        position: relative;
+        left:430px;
+        bottom:300px;
+    }
+    #prev-img{
+        position: relative;
+        left:1377px;
+        bottom:300px;
+    }
+    #navi img{
+        display : block;
+        margin : auto;
+        padding-top:50px;
+    }
+    #navi span{ 
+        background-color: transparent; 
+        color: black; 
+        text-align: center; 
+        border-radius: 50%; 
+        padding: 10px 20px; 
+        top: 50%; 
+        font-size: 1.3em; 
+        cursor: pointer; 
+        background-color: #fff;
+        box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.1);
+    }
+    #navi span:hover{ 
+        background-color: #2EE59D;
+  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+  color: #fff;
+  transform: translateY(-7px);
+    }
+
+</style>
