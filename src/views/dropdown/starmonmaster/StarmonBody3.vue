5bbcc78eec68413256ff6482b06d4c4ae3474518
ic5594 <ic5594@naver.com>
2021-11-12 17:59:10 +0900

    Docs: 데이터 바인딩 값 바인딩 형식 변경 `` -> []

--- src/views/dropdown/starmonmaster/StarmonBody3.vue
+++ src/views/dropdown/starmonmaster/StarmonBody3.vue
@@ -1,22 +1,28 @@
 <template>
     <div id='star-body3'>
-        <Navigate v-bind:navGate="imgUrl"></Navigate>
+        <Navigate v-bind:navGate='imgUrl'></Navigate>
     </div>
 </template>
 <script>
-    import Navigate from '../../../components/navigate.vue'
+import Navigate from '../../../components/navigate.vue'
 
 export default {
-    name:"starbody3",
-    components:{
-        'Navigate':Navigate
-    },
-    data:function(){
-        return{
-            imgUrl:`'https://www.gnigame.co.kr/wp-content/uploads/2018/05/TurnOver-gallery2.jpg','https://www.gnigame.co.kr/wp-content/uploads/2018/04/TurnOver-gallery1-1.jpg'`      
-        }
-    }
-    }
+    components: { 
+            'Navigate':Navigate
+        }, 
+        data: function () { 
+            return { 
+                imgUrl: ['https://www.gnigame.co.kr/wp-content/uploads/2018/05/Starmonmasters-gallery1.jpg',
+               'https://www.gnigame.co.kr/wp-content/uploads/2018/05/Starmonmasters-gallery2.jpg',
+               'https://www.gnigame.co.kr/wp-content/uploads/2018/05/Starmonmasters-gallery3.jpg',
+               'https://www.gnigame.co.kr/wp-content/uploads/2018/05/Starmonmasters-gallery4.jpg' 
+
+                ]
+                }; 
+        },
+
+
+}
 </script>
 <style>
     
