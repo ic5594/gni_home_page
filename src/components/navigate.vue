085f7adcece7d44ccfab8417794d8986b111ee35
ic5594 <ic5594@naver.com>
2021-11-15 17:44:23 +0900

    Style: 파란박스 제거를 위한 user-select값 설정

--- src/components/navigate.vue
+++ src/components/navigate.vue
@@ -6,7 +6,7 @@
             </navi-gate>
                 <span v-on:click="prev" id="next-img">&lang;</span>
                 <span v-on:click="next" id="prev-img">&rang;</span>
-                
+               
         </div>
     </div>
 </template>
@@ -21,7 +21,6 @@ export default {
             timer:null,
         };
     },
-
     methods: {
         prev: function() {
             this.currentNumber += 1
@@ -30,42 +29,52 @@ export default {
             this.currentNumber -= 1
         }
     }
-
 }
 </script>
 <style>
-    #next-img{
-        position: relative;
-        left:430px;
-        bottom:300px;
-    }
-    #prev-img{
-        position: relative;
-        left:1377px;
-        bottom:300px;
-    }
-    #navi img{
-        display : block;
-        margin : auto;
-        padding-top:50px;
-    }
-    #navi span{ 
-        background-color: transparent; 
-        color: black; 
-        text-align: center; 
-        border-radius: 50%; 
-        padding: 10px 20px; 
-        top: 50%; 
-        font-size: 1.3em; 
-        cursor: pointer; 
-        background-color: #fff;
-        box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.1);
-    }
-    #navi span:hover{ 
-        background-color: #2EE59D;
-  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
-  color: #fff;
-  transform: translateY(-7px);
-    }
+#next-img{
+    position: relative;
+    left:430px;
+    bottom:300px;
+    user-select: none; /* standard syntax */
+    -webkit-user-select: none; /* webkit (safari, chrome) browsers */
+    -moz-user-select: none; /* mozilla browsers */
+    -khtml-user-select: none; /* webkit (konqueror) browsers */
+    -ms-user-select: none; /* IE10+ */
+}
+#prev-img{
+    position: relative;
+    left:1377px;
+    bottom:300px;
+    user-select: none; /* standard syntax */
+    -webkit-user-select: none; /* webkit (safari, chrome) browsers */
+    -moz-user-select: none; /* mozilla browsers */
+    -khtml-user-select: none; /* webkit (konqueror) browsers */
+    -ms-user-select: none; /* IE10+ */
+}
+#navi img{
+    display : block;
+    margin : auto;
+     padding-top:50px;
+}
+#navi span{ 
+    background-color: transparent; 
+    color: black; 
+    text-align: center; 
+    border-radius: 50%; 
+    padding: 10px 20px; 
+    top: 50%; 
+    font-size: 1.3em; 
+    cursor: pointer; 
+    background-color: #fff;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.1);
+ }
+#navi span:hover{ 
+    background-color: #2EE59D;
+    box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+    color: #fff;
+    transform: translateY(-7px);
+} 
+
 
 </style>
