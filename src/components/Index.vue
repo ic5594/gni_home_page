1c648e90c333d6750da2e61308dbd848a9f8c923
ic5594 <ic5594@naver.com>
2021-11-15 17:42:32 +0900

    Docs:  footer태그 app.vue로 이동으로 인한 삭제

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,24 +1,17 @@
 <template>
   <div id='index'>
     <Body></Body>
-    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Body from './body/Body.vue'
-import Footer from './footer/Footer.vue'
 
 export default {
   name:'Index',
   components:{
-    'Body':Body,
-    'Footer':Footer
+    'Body':Body
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
