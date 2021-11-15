bf160529eebb19e875d103dd741898a1390d83ff
ic5594 <ic5594@naver.com>
2021-11-15 17:41:03 +0900

    Docs: Footer 컴포넌트 수정을 위해 추가

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,27 @@
   <div id="app">
     <Header></Header>
     <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-
+import Footer from  './components/footer/Footer.vue'
 import Header from './components/header/Header.vue'
 
 export default {
   components:{
-    'Header':Header 
+    'Header':Header,
+    'Footer':Footer
 }
 }
 </script>
+
 <style>
 body{
-margin:0;
-padding:0;
-overflow-x: hidden;
-
+  margin:0;
+  padding:0;
+  overflow-x: hidden;
 }
+
 </style>
